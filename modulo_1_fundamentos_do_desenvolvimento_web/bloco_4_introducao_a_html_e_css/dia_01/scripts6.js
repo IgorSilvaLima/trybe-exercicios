1ad1f7f95059e1325499db0a61f1dad383cc047f
IgorSilvaLima <igor.silva.lima@hotmail.com>
2022-05-12 20:11:33 -0300

    Atividades de aulas do dia 4.1

---
+++ modulo_1_fundamentos_do_desenvolvimento_web/bloco_4_introducao_a_html_e_css/dia_01/scripts6.js
@@ -0,0 +1,17 @@
+let resultado = "lista";
+
+switch (resultado) {
+    case "aprovada":
+        console.log("Parabéns!");
+    break;
+
+    case "lista":
+        console.log("Tenha fé! Você está na lista");
+    break;
+
+    case "reprovada":
+        console.log("Não foi desta vez.");
+    break
+    default:
+        console.log("Valor não identificado")
+}
