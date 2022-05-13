86ecdc8e18aac007ad7c3f5fd5698ae21f1135ae
IgorSilvaLima <igor.silva.lima@hotmail.com>
2022-05-13 20:02:28 -0300

    Exerceicio 1 dia 4.2

--- modulo_1_fundamentos_do_desenvolvimento_web/bloco_4_introducao_a_html_e_css/dia_02/scripts.js
+++ modulo_1_fundamentos_do_desenvolvimento_web/bloco_4_introducao_a_html_e_css/dia_02/scripts.js
@@ -3,3 +3,12 @@ let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for (let nome of names) {
     console.log(nome)
 }
+// Exercícios 1 ao 7
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (var index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index])
+}
+
+
